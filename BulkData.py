9b10c78dc727dd9a0d32fa6b33dcb2f42d58155e
Hugo <hugo.rock20@hotmail.com>
2019-11-26 23:17:34 -0500

    test bulk data

--- BulkData.py
+++ BulkData.py
@@ -1,3 +1,14 @@
 from elasticsearch import Elasticsearch
+import requests
+import json
 
 es = Elasticsearch([{"host": "localhost", "port": 9200}])
+
+i = 1
+r = requests.get('http://localhost:9200') 
+
+while r.status_code == 200:
+    r = requests.get("http://swapi.co/api/people/" + str(i))
+    es.index(index="star_wars", doc_type="people",
+             id=i, body=json.loads(r.content))
+    i = i+1
